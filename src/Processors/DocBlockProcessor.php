2f6c6dd2820134e71fae49faa0f1da31654c5dec
David <davidparedes@nitsnets.com>
2024-04-08 15:56:58 +0200

    feat(docs): added description with tags into json

--- src/Processors/DocBlockProcessor.php
+++ src/Processors/DocBlockProcessor.php
@@ -3,6 +3,7 @@
 namespace AndreasElia\PostmanGenerator\Processors;
 
 use Illuminate\Support\Str;
+use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTextNode;
 use PHPStan\PhpDocParser\Lexer\Lexer;
 use PHPStan\PhpDocParser\Parser\ConstExprParser;
@@ -26,16 +27,40 @@ class DocBlockProcessor
             $comment = $reflectionMethod->getDocComment();
             $tokens = new TokenIterator($lexer->tokenize($comment));
             $phpDocNode = $parser->parse($tokens);
-
+            $tags = [];
             foreach ($phpDocNode->children as $child) {
                 if ($child instanceof PhpDocTextNode) {
                     $description .= ' '.$child->text;
+                }if ($child instanceof PhpDocTagNode) {
+                    //print_r(json_encode($child, 128));
+                    $tags[] = [
+                        'type' => $child->name,
+                        'class' => $this->getTagClass($child)
+                    ];
                 }
             }
-
-            return Str::squish($description);
+            $description = Str::squish($description);
+            foreach ($tags as $tag) {
+                $description .= "\n";
+                $description .= ''.$tag['type'] . ' - ' . $tag['class'];
+            }
+            print_r($description);
+            print_r("\n");
+            return $description;
         } catch (Throwable $e) {
+            print_r($e->getMessage());
             return '';
         }
+    }
+    private function getTagClass($child)
+    {
+        if (isset($child->type)) {
+            $typesResult = '';
+            foreach ($child->type->types as $type) {
+                $typesResult .= $type->name . '|';
+            }
+            return $typesResult;
+        }
+        return $child->value;
     }
 }
